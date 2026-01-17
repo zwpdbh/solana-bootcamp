4c145b8a85d3b2ccaa66566f11fe74f9f3dc381e
zwpdbh <hyperion_z@outlook.com>
2026-01-17 10:07:37 +0800

    learn anchor withdraw

--- task02_anchor_vault/blueshift_anchor_vault/programs/blueshift_anchor_vault/src/lib.rs
+++ task02_anchor_vault/blueshift_anchor_vault/programs/blueshift_anchor_vault/src/lib.rs
@@ -10,7 +10,66 @@ pub mod blueshift_anchor_vault {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
+
+    pub fn deposit(ctx: Context<VaultAction>, amount: u64) -> Result<()> {
+        use anchor_lang::system_program::{transfer, Transfer};
+
+        // 1. verify the vault is empty to prevent double deposits
+        require_eq!(
+            ctx.accounts.vault.lamports(),
+            0,
+            VaultError::VaultAlreadyExists
+        );
+
+        // 2. Ensurethe deposite amout exceeds the rent-exempt minimum for a SystemAccount
+        require_eq!(
+            amount,
+            Rent::get()?.minimum_balance(0),
+            VaultError::InvalidAmount
+        );
+
+        transfer(
+            CpiContext::new(
+                ctx.accounts.system_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.signer.to_account_info(),
+                    to: ctx.accounts.vault.to_account_info(),
+                },
+            ),
+            amount,
+        )?;
+
+        Ok(())
+    }
+    pub fn withdraw(ctx: Context<VaultAction>) -> Result<()> {
+        // withdraw logic
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[derive(Accounts)]
+pub struct VaultAction<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"vault", signer.key().as_ref()],
+        bump,
+    )]
+    // a PDA that holds the lamports for the signer.
+    pub vault: SystemAccount<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[error_code]
+pub enum VaultError {
+    #[msg("Vault already exists")]
+    VaultAlreadyExists,
+    #[msg("Invalid amount")]
+    InvalidAmount,
+}
